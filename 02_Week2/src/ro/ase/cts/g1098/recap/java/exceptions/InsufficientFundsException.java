a8f9f56b4dd3279c1e4da00d11035226ccc1c7d4
kovacsaron19 <kovacsaron19@stud.ase.ro>
2022-03-03 20:09:38 +0200

    Initial stage of the example

---
+++ 02_Week2/src/ro/ase/cts/g1098/recap/java/exceptions/InsufficientFundsException.java
@@ -0,0 +1,10 @@
+package ro.ase.cts.g1098.recap.java.exceptions;
+
+public class InsufficientFundsException extends Exception{
+	public InsufficientFundsException(String msg) {
+		super(msg);
+	}
+	public InsufficientFundsException() {
+		super();
+	}
+}
---
+++ 02_Week2/src/ro/ase/cts/g1098/recap/java/models/Profitable.java
@@ -0,0 +1,7 @@
+package ro.ase.cts.g1098.recap.java.models;
+
+public interface Profitable {
+	
+	public abstract void addInterest(double interestPercentage);
+	
+}
